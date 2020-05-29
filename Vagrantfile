7193068489223a368bb9415dc7f150e0d2c7d4c4
Tocyuki <tocguitar1@gmail.com>
2020-05-29 14:59:49 +0900

    change zsh plugin manager

---
+++ Vagrantfile
@@ -0,0 +1,12 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "bento/ubuntu-18.04"
+  config.vm.synced_folder ".", "/vagrant"
+
+  config.vm.provision :shell, inline: <<-EOT
+    cd /vagrant
+    sh init.sh
+  EOT
+end
---
+++ init.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -eux
+
+if [ $(uname) = "Linux" ]; then
+  add-apt-repository -y ppa:lazygit-team/release
+  add-apt-repository -y ppa:longsleep/golang-backports
+  apt-get update -y
+  apt-get install -y golang-go
+  apt-get install -y lazygit
+
+  apt-get install -y vim
+  apt-get install -y tmux
+  apt-get install -y zsh
+
+  if [ ! -d ${HOME}/.fzf ]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.fzf
+    ${HOME}/.fzf/install
+  fi
+
+    go get github.com/x-motemen/ghq
+elif [ $(uname) = "Drawin" ]; then
+  brew update
+  brew install vim
+  brew install tmux
+  brew install zsh
+  brew install go
+  brew install fzf
+  brew install ghq
+  brew install lazygit
+  brew install docker
+  brew cask install docker
+else
+  echo "This is unsupported OS!! You can use it only Ubuntu or Mac OSX."
+  exit 1
+fi
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+chsh -s $(which zsh)
+
+if [ ! -d ${HOME}/.tmux/plugins/tpm ]; then
+  git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
+fi
+

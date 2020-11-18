147941d88006cfe98d58b6807017642f985e4907
Tocyuki <tocguitar1@gmail.com>
2020-11-18 16:33:41 +0900

    update my dotfiles

--- fish/setup.sh
+++ fish/setup.sh
@@ -5,11 +5,13 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
-fish -C "fisher install jethrokuan/z"
-fish -C "fisher install oh-my-fish/theme-eclm"
-fish -C "fisher install jethrokuan/fzf"
-fish -C "fisher install decors/fish-ghq"
-fish -C "fisher install edc/bass"
+fish -C "\
+fisher install jethrokuan/z;\
+fisher install oh-my-fish/theme-eclm;\
+fisher install jethrokuan/fzf;\
+fisher install decors/fish-ghq;\
+fisher install edc/bass\
+"&
 
 [ -e ${HOME}/.config/fish/config.fish -o -L ${HOME}/.config/fish/config.fish ] && rm -rf ${HOME}/.config/fish/config.fish
 [ ! -L ${HOME}/.config/fish/config.fish ] && ln -s ${SCRIPT_DIR}/config.fish ${HOME}/.config/fish/config.fish
